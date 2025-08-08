34e35f6f86661aeec231c92bb0b14f1a5e64c725
PmisiPmisi <62343642+PmisiPmisi@users.noreply.github.com>
2025-08-08 16:13:50 +0200

    Update script.js
    modositás hogy a lapok mozgathatoak legyenek

--- script.js
+++ script.js
@@ -45,3 +45,17 @@ function init() {
 }
 
 init();
+const kartya = document.getElementById('kartya');
+
+kartya.addEventListener('mousedown', function(e) {
+  function mozgat(event) {
+    kartya.style.left = event.pageX + 'px';
+    kartya.style.top = event.pageY + 'px';
+  }
+
+  document.addEventListener('mousemove', mozgat);
+
+  document.addEventListener('mouseup', function() {
+    document.removeEventListener('mousemove', mozgat);
+  }, { once: true });
+});
